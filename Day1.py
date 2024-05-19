7e90ee76dd18c706ed8ee4cd5c4c45b66e5322ac
thesagaradhikari <110712687+thesagaradhikari@users.noreply.github.com>
2024-05-19 14:26:37 +0545

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,6 @@
+# This is the first day so can be easy as we only started with print function work and 
+#introduced the python as a dyanmic and very useful type of language 
+print("Hello world") 
+print(54)
+print(98," " ,56)
+print("This is kind of easy than most of the languages and I dont know why!!")
