f3be60fc0026044e20e9e6611f917e04d461194a
thesagaradhikari <110712687+thesagaradhikari@users.noreply.github.com>
2024-06-11 07:17:25 +0545

    Day 4 is updated

---
+++ Day5/main.py
@@ -0,0 +1,3 @@
+print("Hey I am a good boy\nand this viewer is also a good boy/girl")
+# comments is done through this symbol
+# This is the 5th day of 100 days of coding 
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 import hashlib
 print("hi")
+print("print hello in the terminal")
+print(7)
+print(17*13)    
