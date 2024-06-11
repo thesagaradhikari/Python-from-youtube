d7c0c31d5743ca735ef670ff06c41644de724d59
thesagaradhikari <110712687+thesagaradhikari@users.noreply.github.com>
2024-06-11 22:47:18 +0545

    day 5 update

--- Day5/main.py
+++ Day5/main.py
@@ -1,3 +1,8 @@
 print("Hey I am a good boy\nand this viewer is also a good boy/girl")
-# comments is done through this symbol
 # This is the 5th day of 100 days of coding 
+# comments is done through this symbol
+print("This is \"this is inside the double coute \"a print statement.")#print function
+"""
+this is the comment that is done through the three 
+"""
+print("Hey",6,7,sep="--")
