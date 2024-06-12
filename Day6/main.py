6737315114fcc00dbd42704c6b5bf7c4fa73a032
thesagaradhikari <110712687+thesagaradhikari@users.noreply.github.com>
2024-06-12 23:11:47 +0545

    day 6 done

---
+++ Day6/main.py
@@ -0,0 +1,15 @@
+#data types and variables 
+a=12
+print(a)
+b="Sagar"
+print(b)
+print("the type of b is ",type(b))
+print("the type of a is ",type(a))
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+print(dict1)
