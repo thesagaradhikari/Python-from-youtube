94487be810e3703b9bc44a634aca869b4e116376
thesagaradhikari <110712687+thesagaradhikari@users.noreply.github.com>
2024-06-14 22:26:45 +0545

    Day 6 done

---
+++ Day7/main.py
@@ -0,0 +1,6 @@
+#calculator to divide two number
+print("WRITE the value of two number to be divided")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("The division of two number is: ")
+print(num1/num2)
